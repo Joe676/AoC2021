0cfcefe0928a5516d692e4baf7c8aaf149fae6d4
Joe676 <bossowski92@gmail.com>
2021-12-01 08:59:16 +0100

    Added comments

--- 1/1.py
+++ 1/1.py
@@ -1,3 +1,11 @@
+"""
+Solution for exercise 1 of Advent of Code 2021
+
+In order to find the number of times the depth gets bigger
+it iterates over all the measurements and compares each one
+to the previous one - incrementing count each time we go deeper.
+"""
+
 def main():
     f = open("1_input.txt", "r")
     count = 0
