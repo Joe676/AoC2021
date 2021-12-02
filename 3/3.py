c87ff1c100c225a93bce9df605401b18a510bb31
Joe676 <bossowski92@gmail.com>
2021-12-02 10:12:46 +0100

    Added comments and configured main functions

--- 3/3.py
+++ 3/3.py
@@ -1,20 +1,30 @@
-f = open("3/3_input.txt", "r")
-lines = f.readlines()
-f.close()
-x = 0
-y = 0
+"""
+Solution for exercise 3 of Advent of Code 2021
 
-for line in lines:
-    line.rstrip()
-    l = line.split(" ")
-    print(l)
-    opt = l[0]
-    print(l[1])
-    h = int(l[1])
-    if opt == "forward":
-        x+=h
-    elif opt == "down":
-        y+=h
-    elif opt == "up":
-        y-=h
-print(x, y, x*y)
+Each line of input is parsed and then variables x and y are updated accordingly.
+"""
+
+def main():
+    f = open("3/3_input.txt", "r")
+    lines = f.readlines()
+    f.close()
+    x = 0
+    y = 0
+
+    for line in lines:
+        line.rstrip()
+        l = line.split(" ")
+        opt = l[0]
+        h = int(l[1])
+        if opt == "forward":
+            x+=h
+        elif opt == "down":
+            y+=h
+        elif opt == "up":
+            y-=h
+    return (x, y)
+
+
+if __name__ == "__main__":
+    x, y = main()
+    print(x, y, x*y)
--- 4/4.py
+++ 4/4.py
@@ -1,20 +1,31 @@
-f = open("4/4_input.txt", "r")
-lines = f.readlines()
-f.close()
+"""
+Solution for exercise 3 of Advent of Code 2021
 
-aim = 0
-x, y = 0, 0
+Each line of input is parsed and then variables x, y and aim are updated accordingly.
+"""
 
-for line in lines:
-    line.rstrip()
-    l = line.split(" ")
-    opt = l[0]
-    h = int(l[1])
-    if opt == "forward":
-        x+=h
-        y+=h*aim
-    elif opt == "down":
-        aim+=h
-    elif opt == "up":
-        aim-=h
-print(x, y, x*y)
+def main():
+    f = open("4/4_input.txt", "r")
+    lines = f.readlines()
+    f.close()
+
+    aim = 0
+    x, y = 0, 0
+
+    for line in lines:
+        line.rstrip()
+        l = line.split(" ")
+        opt = l[0]
+        h = int(l[1])
+        if opt == "forward":
+            x+=h
+            y+=h*aim
+        elif opt == "down":
+            aim+=h
+        elif opt == "up":
+            aim-=h
+    return (x, y)
+
+if __name__ == "__main__":
+    x, y = main()
+    print(x, y, x*y)
